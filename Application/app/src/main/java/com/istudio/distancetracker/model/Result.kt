641ca6e8544f1933ff2874e8c8ec1fec65666c0f
Devrath <devrath@mplgaming.com>
2022-12-22 19:15:11 +0530

    added more code

--- Application/app/src/main/java/com/istudio/distancetracker/model/Result.kt
+++ Application/app/src/main/java/com/istudio/distancetracker/model/Result.kt
@@ -1,9 +1,11 @@
 package com.istudio.distancetracker.model
 
 import android.os.Parcelable
+import androidx.annotation.Keep
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
+@Keep
 data class Result(
         var distance: String,
         var time: String
