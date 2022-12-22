04f09df36af7265f5696e87bc97f984c971bb275
Devrath <devrath@mplgaming.com>
2022-12-22 11:36:05 +0530

    refactor

--- Application/buildSrc/src/main/java/Google.kt
+++ Application/buildSrc/src/main/java/Google.kt
@@ -1,6 +1,6 @@
 object Google {
     private const val materialVersion = "1.4.0"
-    private const val playCoreVersion = "1.6.4"
+    private const val playCoreVersion = "1.10.3"
     const val material = "com.google.android.material:material:$materialVersion"
     const val playCore = "com.google.android.play:core:$playCoreVersion"
 }
