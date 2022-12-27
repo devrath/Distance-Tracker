cd989c036ede2c6518069e6f706ba7a845b6729d
Devrath <devrath@mplgaming.com>
2022-12-27 16:42:04 +0530

    new changes

---
+++ Application/core/core-common/src/main/java/com/istudio/core_common/di/qualifiers/Dispatchers.kt
@@ -0,0 +1,15 @@
+package com.mpl.androidapp.updater.downloadmanager.di.qualifiers
+
+import javax.inject.Qualifier
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class DefaultDispatcher
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class IoDispatcher
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class MainDispatcher
