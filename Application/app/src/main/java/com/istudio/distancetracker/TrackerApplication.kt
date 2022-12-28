f069d1c56abd133bf11de0324499ffc09f164f7d
Devrath <devrath@mplgaming.com>
2022-12-28 22:02:09 +0530

    logger module removed

--- Application/app/src/main/java/com/istudio/distancetracker/TrackerApplication.kt
+++ Application/app/src/main/java/com/istudio/distancetracker/TrackerApplication.kt
@@ -2,7 +2,6 @@ package com.istudio.distancetracker
 
 import android.app.Application
 import com.istudio.core_logger.ApplicationLoggerConfig
-import com.istudio.distancetracker.core.data.implementation.logger.utilities.AppLoggerConfig
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -10,7 +9,6 @@ class TrackerApplication: Application() {
     override fun onCreate() {
         super.onCreate()
         if (BuildConfig.DEBUG) {
-            AppLoggerConfig(this).initializeLogging()
             // New logger
             ApplicationLoggerConfig(this).initializeLogging(isEnabled = true)
         }
