4652f51128602133647474533bc3841a3c9600f2
Devrath <devrath@mplgaming.com>
2022-12-30 12:55:31 +0530

    increased the version

--- Application/buildSrc/src/main/java/ProjectConfig.kt
+++ Application/buildSrc/src/main/java/ProjectConfig.kt
@@ -3,7 +3,7 @@ object ProjectConfig {
     const val compileSdk = 33
     const val minSdk = 21
     const val targetSdk = 33
-    const val versionCode = 13
+    const val versionCode = 14
     const val versionName = "1.4"
     const val jvmTarget = "1.8"
     const val testRunner = "com.istudio.distancetracker.TestRunner"
